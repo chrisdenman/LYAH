ac073f8d9f53989f6e5c590de9be78dfadf44cd4
Chris Denman <chrisdenman@icloud.com>
2024-06-05 23:15:19 +0100

    Completed chapter 7 exercises.

--- docker-ubuntu-haskell/docker/ubuntu-haskell/learn-you-a-haskell-exercises/solutions/cdenman/DistanceConversions.hs
+++ docker-ubuntu-haskell/docker/ubuntu-haskell/learn-you-a-haskell-exercises/solutions/cdenman/DistanceConversions.hs
@@ -2,16 +2,25 @@ module DistanceConversions
 ( yardsToFeet
 , feetToInches
 , inchesToCentimetres
+, centimetresToMetres
+, chainsToYards
 ) where
+
+-- Define chains to yards
+chainsToYards ::  Float -> Float
+chainsToYards y = y * 22
 
 -- Define yards to feet
 yardsToFeet ::  Float -> Float
-yardsToFeet y = undefined
+yardsToFeet y = y * 3
 
 -- Define feet to inches
 feetToInches :: Float -> Float
-feetToInches f = undefined
+feetToInches f = f * 12
 
 -- Define inches to centimetres
 inchesToCentimetres :: Float -> Float
-inchesToCentimetres i = undefined
+inchesToCentimetres inches = 254 * inches / 100 -- error on this line in the original 'i' vs 'inches'
+
+centimetresToMetres :: Float -> Float
+centimetresToMetres cm = cm / 100
